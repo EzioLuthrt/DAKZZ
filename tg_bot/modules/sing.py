146fca7e035010e1e558e7f412ce6a572093efc7
ABBAS MHD SALIM <60001289+ABBASMHDSALIMZDX@users.noreply.github.com>
2020-03-10 07:40:36 +0000

    Add files via upload

---
+++ tg_bot/modules/sing.py
@@ -0,0 +1,29 @@
+import random
+from telegram.ext import run_async, Filters
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+SFW_STRINGS = (
+    "🎶 മിഴിയറിയാതെ വന്നു നീ മിഴിയൂഞ്ഞാലിൽ... കനവറിയാതെയേതോ കിനാവു പോലെ... 🎶.",
+    "🎶 നിലാവിന്റെ നീലഭസ്മ കുറിയണിഞ്ഞവളേ... കാതിലോലക്കമ്മലിട്ടു കുണുങ്ങി നിന്നവളേ... 🎶",
+  )
+
+@run_async
+def sing(bot: Bot, update: Update):
+    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
+    else:
+      message.reply_text(random.choice(SFW_STRINGS))
+
+__help__ = """
+- /sing  ചില മലയാളം പാട്ടുകളുടെ ആദ്യ വരികൾ ലഭിക്കും.
+"""
+
+__mod_name__ = "Sing Malayalam"
+
+SING_HANDLER = DisableAbleCommandHandler("sing", sing)
+
+dispatcher.add_handler(SING_HANDLER)
